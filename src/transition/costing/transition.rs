964513fc749f6b1d24a4599b210e0ceb3eed9243
Ben White <contact@bennjii.dev>
2025-10-06 22:41:45 +0200

    feat(costing): easier to rationalize costing mechanisms 2

--- src/transition/costing/transition.rs
+++ src/transition/costing/transition.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::LN_2;
+
 use crate::transition::candidate::{Candidate, CandidateId};
 use crate::transition::{ResolutionMethod, RoutingContext, Strategy, Trip, VirtualTail};
 use geo::{Distance, Haversine};
@@ -42,6 +44,7 @@ where
     pub requested_resolution_method: ResolutionMethod,
 }
 
+#[derive(Debug)]
 pub struct TransitionLengths {
     /// The great circle distance between source and target
     pub straightline_distance: f64,
@@ -51,6 +54,8 @@ pub struct TransitionLengths {
 }
 
 impl TransitionLengths {
+    /// todo: update me to reflect current content.
+    ///
     /// Calculates the deviance in straightline distance to the length
     /// of the entire route. Returns values between 0 and 1. Where values
     /// closer to 1 represent more optimal distances, whilst those closer
@@ -76,11 +81,10 @@ impl TransitionLengths {
     /// Note that a lower deviance score means the values are less aligned.
     #[inline]
     pub fn deviance(&self) -> f64 {
-        let deviance = self.route_length - self.straightline_distance;
-        (deviance / self.straightline_distance)
-            .abs()
-            .powi(2)
-            .clamp(0.0, 1.0)
+        const HALVING_FACTOR: f64 = LN_2;
+
+        let denom = HALVING_FACTOR + (self.route_length / self.straightline_distance).ln();
+        (HALVING_FACTOR / denom).clamp(0.0, 1.0)
     }
 }
 
