b6c63d5654b9b7b4c2c061a7d6c1fc64cb50e827
Ben White <contact@bennjii.dev>
2026-05-02 18:57:18 +1000

    fix(examples): simplify simple one further

--- examples/map_match.rs
+++ examples/map_match.rs
@@ -1,26 +1,19 @@
-use geo::LineString;
+use geo::{LineString, Point};
 use routers::r#match::MatchSimpleExt;
 use routers_codec::osm::OsmNetwork;
 use wkt::TryFromWkt;
 
-use routers_fixtures::{SYDNEY, SYDNEY_SAVED, SYNDEY_TRIP, fixture};
+use routers_fixtures::{SYDNEY, SYNDEY_TRIP, fixture};
 
 fn main() {
     let coordinates: LineString<f64> =
         LineString::try_from_wkt_str(SYNDEY_TRIP).expect("must parse");
 
     let pbf_path = fixture!(SYDNEY);
-    let saved_path = fixture!(SYDNEY_SAVED);
-
-    if !saved_path.exists() {
-        let graph = OsmNetwork::from_pbf(pbf_path).expect("Graph must be created");
-        graph.save_to_file(saved_path).expect("must save to file");
-    }
-
-    let graph = OsmNetwork::from_saved(saved_path).expect("Graph must be created");
+    let graph = OsmNetwork::from_pbf(pbf_path).expect("Graph must be created");
 
     let route = graph
-        .r#match_simple(coordinates.clone())
+        .r#match_simple(coordinates)
         .expect("Match must complete successfully");
 
     let linestring = route
