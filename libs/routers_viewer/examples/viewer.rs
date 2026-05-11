e82a23b730fc36b362de4efc6cf1e0fbd07ca369
Ben White <contact@bennjii.dev>
2026-05-11 22:30:16 +1000

    fix(bench): update again

--- libs/routers_viewer/examples/viewer.rs
+++ libs/routers_viewer/examples/viewer.rs
@@ -2,22 +2,29 @@ use routers_viewer::ViewerApp;
 
 use eframe::NativeOptions;
 use routers_codec::osm::OsmNetwork;
-use routers_fixtures::{SYDNEY, SYDNEY_SAVED, fixture};
+use routers_fixtures::{LOS_ANGELES, LOS_ANGELES_SAVED, fixture};
+use tokio::time::Instant;
 
 #[tokio::main]
 async fn main() -> eframe::Result<()> {
     env_logger::init();
     let native_options = NativeOptions::default();
 
-    let pbf_path = fixture!(SYDNEY);
-    let saved_path = fixture!(SYDNEY_SAVED);
+    let pbf_path = fixture!(LOS_ANGELES);
+    let saved_path = fixture!(LOS_ANGELES_SAVED);
 
     if !saved_path.exists() {
         let graph = OsmNetwork::from_pbf(pbf_path).expect("Graph must be created");
         graph.save_to_file(saved_path).expect("must save to file");
     }
 
-    let network = OsmNetwork::from_saved(fixture!(SYDNEY_SAVED)).expect("Graph must be created");
+    println!("File ready! Opening...");
+    let now = Instant::now();
+
+    let network =
+        OsmNetwork::from_saved(fixture!(LOS_ANGELES_SAVED)).expect("Graph must be created");
+
+    println!("Openened in {:?}", now.elapsed());
 
     eframe::run_native(
         "Routers Map Matcher",
