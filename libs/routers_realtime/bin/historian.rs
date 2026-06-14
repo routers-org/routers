410987fe8699fab91cbc3a2878fbac9f97e6f02d
Ben White <contact@bennjii.dev>
2026-06-14 14:00:24 +1000

    feat(realtime): historian, matcher and orchestrator binaries

---
+++ libs/routers_realtime/bin/historian.rs
@@ -0,0 +1,7 @@
+use log::info;
+
+fn main() -> anyhow::Result<()> {
+    info!("historian started");
+
+    Ok(())
+}
---
+++ libs/routers_realtime/bin/matcher.rs
@@ -0,0 +1,7 @@
+use log::info;
+
+fn main() -> anyhow::Result<()> {
+    info!("matcher started");
+
+    Ok(())
+}
